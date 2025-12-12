f737cba81de5f3caa17dfda3c2a0fcd462689cb8
Xiaohan Ma <maxiaohan96@gmail.com>
2025-12-12 02:46:10 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -107,9 +107,7 @@ function initScrollAnimations() {
         threshold: 0.1,
         rootMargin: '0px 0px -50px 0px'
     };
-    
-    const animatedElements = document.querySelectorAll('.interest-card');
-    
+        
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
